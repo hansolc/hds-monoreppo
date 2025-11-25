fdbcf487ecf97b4a5c3e46c7c0c2a4222cb83a1e
hansolc <hsc080808@naver.com>
2025-11-25 17:29:20 +0900

    🔧 Add Components style

--- apps/storybook/.storybook/preview.tsx
+++ apps/storybook/.storybook/preview.tsx
@@ -2,6 +2,7 @@ import type { Preview } from "@storybook/react";
 import { withThemeByClassName } from "@storybook/addon-themes";
 import { lightThemeClass, darkThemeClass } from "@repo/design-system/styles";
 import "@repo/design-system/theme.css";
+import "@repo/design-system/components.css";
 
 const preview: Preview = {
   decorators: [
